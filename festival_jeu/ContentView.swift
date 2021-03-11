a658af031964aa08ff17a75af8417a9a54c8847c
Bryan988 <GITHUB@linh2005>
2021-03-11 14:37:36 +0100

    Add All files

---
+++ festival_jeu/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  festival_jeu
+//
+//  Created by user190484 on 11/03/2021.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ festival_jeu/festival_jeuApp.swift
@@ -0,0 +1,18 @@
+//
+//  festival_jeuApp.swift
+//  festival_jeu
+//
+//  Created by user190484 on 11/03/2021.
+//
+//
+
+import SwiftUI
+
+@main
+struct festival_jeuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
